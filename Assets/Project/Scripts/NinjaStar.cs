563939e7696427f6eb3801dba9a76c4abca9212e
tomer <tomerkubovsky@gmail.com>
2021-02-09 19:05:34 +1100

    TO PULL I DID NOTHING MUCH

--- Assets/Project/Scripts/NinjaStar.cs
+++ Assets/Project/Scripts/NinjaStar.cs
@@ -15,4 +15,9 @@ public class NinjaStar : MonoBehaviour
     {
         transform.position += transform.right*-1/2;
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hi");
+        transform.forward = Vector3.Reflect(transform.forward, collision.transform.position.normalized);
+    }
 }
